71b95d213d80e41da353822a590ab0a80490b0b3
MuhleSizani <muhlesizanifx@gmail.com>
2025-04-18 23:12:39 +0200

    Add files via upload

---
+++ Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Client.Models
+{
+    public class Event
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Event name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Event date is required.")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Event location is required.")]
+        public string Location { get; set; }
+
+
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventEase.Client.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "User name is required.")]
+        public string UserName { get; set; }
+    }
+}
